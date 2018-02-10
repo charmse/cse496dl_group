983ee3c4aa0f865145f178c97124ed23045bb249
charmse <caleharms@protonmail.ch>
2018-02-10 13:31:18 -0600

    update basic_cale.py

--- homework/01/basic_cale.py
+++ homework/01/basic_cale.py
@@ -6,7 +6,7 @@ import os
 
 flags = tf.app.flags
 flags.DEFINE_string('data_dir', '/work/cse496dl/shared/homework/01/', 'directory where MNIST is located')
-flags.DEFINE_string('save_dir', 'hackathon_3', 'directory where model graph and weights are saved')
+flags.DEFINE_string('save_dir', '/work/soh/charms/cse496dl/homework/01/basic/', 'directory where model graph and weights are saved')
 flags.DEFINE_integer('batch_size', 32, '')
 flags.DEFINE_integer('max_epoch_num', 100, '')
 FLAGS = flags.FLAGS
@@ -58,115 +58,118 @@ def main(argv):
     validation_num_examples = validation_images.shape[0]
     train_num_examples = train_images.shape[0]
     
-    ovrall_best_accuracy = float("inf")
-    combos = [p for p in itr.product([100,200,300,400,500,600], repeat=2)]
-    for c in combos:
-        tf.reset_default_graph()
-        # specify the network
-        input_placeholder = tf.placeholder(tf.float32, [None, 784], name='data')
-        input_norm = input_placeholder/255
-        KEEP_PROB = 0.5
+    # specify the network
+    input_placeholder = tf.placeholder(tf.float32, [None, 784], name='data')
+    input_norm = input_placeholder/255
+    KEEP_PROB = 0.5
 
-        dropped_input = tf.layers.dropout(input_norm, KEEP_PROB)
-        hidden_1 = tf.layers.dense(dropped_input,
-                                    c[0],
-                                    activation=tf.nn.relu,
-                                    name='hidden_layer_1')
-        dropped_hidden_1 = tf.layers.dropout(hidden_1, KEEP_PROB)
-        hidden_2 = tf.layers.dense(dropped_hidden_1,
-                                    c[1],
-                                    activation=tf.nn.relu,
-                                    name='hidden_layer_2')
-        dropped_hidden_2 = tf.layers.dropout(hidden_2, KEEP_PROB)
-        output = tf.layers.dense(dropped_hidden_2,
-                                    10,
-                                    name='output_layer')
-        tf.identity(output, name='model_output')
+    dropped_input = tf.layers.dropout(input_norm, KEEP_PROB)
+    hidden_1 = tf.layers.dense(dropped_input,
+                                400,
+                                activation=tf.nn.relu,
+                                name='hidden_layer_1')
+    dropped_hidden_1 = tf.layers.dropout(hidden_1, KEEP_PROB)
+    hidden_2 = tf.layers.dense(dropped_hidden_1,
+                                400,
+                                activation=tf.nn.relu,
+                                name='hidden_layer_2')
+    dropped_hidden_2 = tf.layers.dropout(hidden_2, KEEP_PROB)
+    output = tf.layers.dense(dropped_hidden_2,
+                                10,
+                                name='output_layer')
+    tf.identity(output, name='model_output')
 
-        # define classification loss
-        y = tf.placeholder(tf.float32, [None, 10], name='label')
-        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output)
-        red_mean = tf.reduce_mean(cross_entropy)
-        confusion_matrix_op = tf.confusion_matrix(tf.argmax(y, axis=1), tf.argmax(output, axis=1), num_classes=10)
-        accuracy_op = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(output, axis=1), tf.argmax(y, axis=1)) , tf.float32))
+    # define classification loss
+    y = tf.placeholder(tf.float32, [None, 10], name='label')
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=output)
+    red_mean = tf.reduce_mean(cross_entropy)
+    confusion_matrix_op = tf.confusion_matrix(tf.argmax(y, axis=1), tf.argmax(output, axis=1), num_classes=10)
+    accuracy_op = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(output, axis=1), tf.argmax(y, axis=1)) , tf.float32))
 
 
-        # set up training and saving functionality
-        global_step_tensor = tf.get_variable('global_step', trainable=False, shape=[], initializer=tf.zeros_initializer)
-        optimizer = tf.train.AdamOptimizer()
-        train_op = optimizer.minimize(cross_entropy, global_step=global_step_tensor)
-        validate_op = optimizer.minimize(cross_entropy, global_step=global_step_tensor)
-        saver = tf.train.Saver()
+    # set up training and saving functionality
+    global_step_tensor = tf.get_variable('global_step', trainable=False, shape=[], initializer=tf.zeros_initializer)
+    optimizer = tf.train.AdamOptimizer()
+    train_op = optimizer.minimize(cross_entropy, global_step=global_step_tensor)
+    validate_op = optimizer.minimize(cross_entropy, global_step=global_step_tensor)
+    saver = tf.train.Saver()
 
-        with tf.Session() as session:
-            session.run(tf.global_variables_initializer())
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
 
-            # run training
-            batch_size = FLAGS.batch_size
-            best_validation_ce = float("inf")
-            best_accuracy = float("inf")
-            count = 0
-            for epoch in range(FLAGS.max_epoch_num):
+        # run training
+        batch_size = FLAGS.batch_size
+        best_validation_ce = float("inf")
+        best_accuracy_ = float("inf")
+        count = 0
+        for epoch in range(FLAGS.max_epoch_num):
 
-                # run gradient steps and report mean loss on train data
-                ce_vals = []
-                for i in range(train_num_examples // batch_size):
-                    batch_xs = train_images[i*batch_size:(i+1)*batch_size, :]
-                    batch_ys = train_labels[i*batch_size:(i+1)*batch_size, :]       
-                    _, train_ce = session.run([train_op, red_mean], {input_placeholder: batch_xs, y: batch_ys})
-                    ce_vals.append(train_ce)
-                avg_train_ce = sum(ce_vals) / len(ce_vals)
+            # run gradient steps and report mean loss on train data
+            ce_vals = []
+            for i in range(train_num_examples // batch_size):
+                batch_xs = train_images[i*batch_size:(i+1)*batch_size, :]
+                batch_ys = train_labels[i*batch_size:(i+1)*batch_size, :]       
+                _, train_ce = session.run([train_op, red_mean], {input_placeholder: batch_xs, y: batch_ys})
+                ce_vals.append(train_ce)
+            avg_train_ce = sum(ce_vals) / len(ce_vals)
 
-                accuracy_vals = []
-                ce_vals = []
-                conf_mxs = []
-                for i in range(validation_num_examples // batch_size):
-                    batch_xs = validation_images[i*batch_size:(i+1)*batch_size, :]
-                    batch_ys = validation_labels[i*batch_size:(i+1)*batch_size, :]       
-                    validate_ce, conf_matrix, accuracy = session.run([red_mean, confusion_matrix_op, accuracy_op], {input_placeholder: batch_xs, y: batch_ys})
-                    ce_vals.append(validate_ce)
-                    conf_mxs.append(conf_matrix)
-                    accuracy_vals.append(accuracy)
-                avg_validation_ce = sum(ce_vals) / len(ce_vals)
-                avg_accuracy = sum(accuracy_vals) / len(accuracy_vals)
+            accuracy_vals = []
+            ce_vals = []
+            conf_mxs = []
+            for i in range(validation_num_examples // batch_size):
+                batch_xs = validation_images[i*batch_size:(i+1)*batch_size, :]
+                batch_ys = validation_labels[i*batch_size:(i+1)*batch_size, :]       
+                validate_ce, conf_matrix, accuracy = session.run([red_mean, confusion_matrix_op, accuracy_op], {input_placeholder: batch_xs, y: batch_ys})
+                ce_vals.append(validate_ce)
+                conf_mxs.append(conf_matrix)
+                accuracy_vals.append(accuracy)
+            avg_validation_ce = sum(ce_vals) / len(ce_vals)
+            avg_accuracy = sum(accuracy_vals) / len(accuracy_vals)
 
-                if avg_validation_ce < best_validation_ce:
-                    best_validation_ce = avg_validation_ce
-                    best_epoch = epoch
-                    best_train_ce = avg_train_ce
-                    best_conf_mx = sum(conf_mxs)
-                    best_accuracy = avg_accuracy
-                    best_model = saver.save(session, os.path.join(FLAGS.save_dir, "homework_1-0"))
-                    count = 0
-                else:
-                    count += 1
-                
-                if count > 12 :
-                    break
+            with open('/work/soh/charms/cse496dl/homework/01/basic/model_out.txt', "a") as myfile:
+                myfile.write("epoch: " + str(epoch) +
+                "\ntrain loss: " + str(avg_train_ce) +
+                "\nvalidation LOSS: " + str(avg_validation_ce) +
+                "\naccuracy: " + str(avg_accuracy) +
+                "\n------------------------------")
 
-            with open('/work/soh/charms/csce496/hw/hw01/modelParamsAndOut.txt', "a") as myfile:
-                myfile.write("EPOCH: " + str(best_epoch) +
-                "\nHIDDEN STRUCTURE: " + str(c) +
-                "\nTRAIN LOSS: " + str(best_train_ce) +
-                "\nVALIDATION LOSS: " + str(best_validation_ce) +
-                "\nACCURACY: " + str(best_accuracy) +
-                "\nCONFUSION MATRIX: " + str(best_conf_mx))
+            if avg_validation_ce < best_validation_ce:
+                best_validation_ce = avg_validation_ce
+                best_epoch = epoch
+                best_train_ce = avg_train_ce
+                best_conf_mx = sum(conf_mxs)
+                best_accuracy = avg_accuracy
+                best_model = saver.save(session, os.path.join(FLAGS.save_dir, "homework_1-0_val"))
+                count = 0
+            else:
+                count += 1
 
-            if best_accuracy < ovrall_best_accuracy:
-                    ovrall_best_validation_ce = best_validation_ce
-                    ovrall_best_train_ce = best_train_ce
-                    ovrall_best_conf_mx = best_conf_mx
-                    ovrall_best_accuracy = avg_accuracy
-                    ovrall_best_model = best_model
-                    best_combo = c
+            if avg_accuracy < best_accuracy_:
+                best_validation_ce_ = avg_validation_ce
+                best_epoch_ = epoch
+                best_train_ce_ = avg_train_ce
+                best_conf_mx_ = sum(conf_mxs)
+                best_accuracy_ = avg_accuracy
+                best_model_ = saver.save(session, os.path.join(FLAGS.save_dir, "homework_1-0_acu"))
+                count = 0
+            
+            if count > 12 :
+                break
 
-        with open('/work/soh/charms/csce496/hw/01/combo_params)and_out.txt', "a") as myfile:
-            myfile.write("BEST STRUCTURE: " + str(best_combo) +
-            "\nBEST TRAIN LOSS: " + str(ovrall_best_train_ce) +
-            "\nBEST VALIDATION LOSS: " + str(ovrall_best_validation_ce) +
-            "\nBEST ACCURACY: " + str(ovrall_best_accuracy) +
-            "\nBEST CONFUSION MATRIX: " + str(ovrall_best_conf_mx) +
-            "\nBEST MODEL: " + str(ovrall_best_model))
+        with open('/work/soh/charms/cse496dl/homework/01/basic/model_out.txt', "a") as myfile:
+            myfile.write("EPOCH: " + str(best_epoch) +
+            "\nTRAIN LOSS: " + str(best_train_ce) +
+            "\nVALIDATION LOSS: " + str(best_validation_ce) +
+            "\nACCURACY: " + str(best_accuracy) +
+            "\nCONFUSION MATRIX: " + str(best_conf_mx) +
+            "\n------------------------------------------")
+
+        with open('/work/soh/charms/cse496dl/homework/01/basic/model_out.txt', "a") as myfile:
+            myfile.write("EPOCH: " + str(best_epoch_) +
+            "\nTRAIN LOSS: " + str(best_train_ce_) +
+            "\nVALIDATION LOSS: " + str(best_validation_ce_) +
+            "\nACCURACY: " + str(best_accuracy_) +
+            "\nCONFUSION MATRIX: " + str(best_conf_mx_))
         
 
 
