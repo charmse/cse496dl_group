ed486294af1f55b1fb49f467f297f19c33e6ec36
charmse <caleharms@protonmail.ch>
2018-03-11 21:36:40 -0500

    Add validation option

--- homework/03/main.py
+++ homework/03/main.py
@@ -21,6 +21,7 @@ flags.DEFINE_string('transfer', '', '')
 flags.DEFINE_string('ae', '', '')
 flags.DEFINE_integer('code_size', 100, '')
 flags.DEFINE_float('sparsity_weight', 5e-3, '')
+flags.DEFINE_boolean('validate', False, '')
 FLAGS = flags.FLAGS
 
 def main(argv):
@@ -37,6 +38,7 @@ def main(argv):
     ae = FLAGS.ae
     code_size = FLAGS.code_size
     sparsity_weight = FLAGS.sparsity_weight
+    validate = FLAGS.validate
     if FLAGS.db == "savee":
         data_dir = FLAGS.data_dir + "SAVEE-British/"
         save_prefix = "savee_"
@@ -68,7 +70,11 @@ def main(argv):
     test_labels = np.load(data_dir + 'y_test.npy')
 
     # split into train and validate
-    train_images, valid_images, train_labels, valid_labels = util.split_data(train_images, train_labels, split)
+    if validate:]
+        train_images, valid_images, train_labels, valid_labels = util.split_data(train_images, train_labels, split)
+        valid_num_examples = valid_images.shape[0]
+    train_num_examples = train_images.shape[0]
+    test_num_examples = test_images.shape[0]
 
     # define classification loss
     y = tf.placeholder(tf.float32, [None, 100], name='label')
@@ -83,10 +89,6 @@ def main(argv):
         # setup optimizer
         optimizer = tf.train.AdamOptimizer()
         train_op = optimizer.minimize(total_loss)
-
-        train_num_examples = train_images.shape[0]
-        valid_num_examples = valid_images.shape[0]
-        test_num_examples = test_images.shape[0]
 
         encoder_saver = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='encoder'))
         decoder_saver = tf.train.Saver(var_list=tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='decoder'))
@@ -106,27 +108,28 @@ def main(argv):
                     session.run(train_op, {x: batch_xs})
                 
                 #Validate
-                valid_loss = []
-                for i in range(valid_num_examples // batch_size):
-                    batch_xs = valid_images[i*batch_size:(i+1)*batch_size, :]
-                    loss = session.run(total_loss, {x: batch_xs})
-                    valid_loss.append(loss)
-                avg_valid_loss = np.sum(valid_loss) / len(valid_loss)
-                
-                #Early Stopping
-                if avg_valid_loss < best_valid_loss:
-                    best_valid_loss = avg_valid_loss
-                    best_epoch = epoch
-                    count = 0
-                    encoder_saver.save(session, os.path.join(save_dir + "models/", "maxquality_encoder_homework_3-0"))
-                    decoder_saver.save(session, os.path.join(save_dir + "models/", "maxquality_decoder_homework_3-0"))
-                    encoder_saver.save(session, os.path.join(save_dir + "models/", "maxcompression_encoder_homework_3-0"))
-                    decoder_saver.save(session, os.path.join(save_dir + "models/", "maxcompression_decoder_homework_3-0"))
-                else:
-                    count += 1
+                if validate:
+                    valid_loss = []
+                    for i in range(valid_num_examples // batch_size):
+                        batch_xs = valid_images[i*batch_size:(i+1)*batch_size, :]
+                        loss = session.run(total_loss, {x: batch_xs})
+                        valid_loss.append(loss)
+                    avg_valid_loss = np.sum(valid_loss) / len(valid_loss)
+                    
+                    #Early Stopping
+                    if avg_valid_loss < best_valid_loss:
+                        best_valid_loss = avg_valid_loss
+                        best_epoch = epoch
+                        count = 0
+                        encoder_saver.save(session, os.path.join(save_dir + "models/", "maxquality_encoder_homework_3-0"))
+                        decoder_saver.save(session, os.path.join(save_dir + "models/", "maxquality_decoder_homework_3-0"))
+                        encoder_saver.save(session, os.path.join(save_dir + "models/", "maxcompression_encoder_homework_3-0"))
+                        decoder_saver.save(session, os.path.join(save_dir + "models/", "maxcompression_decoder_homework_3-0"))
+                    else:
+                        count += 1
 
-                if count > early_stop:
-                    break
+                    if count > early_stop:
+                        break
 
             #Run a test
             psnr_list = []
@@ -134,7 +137,13 @@ def main(argv):
                 x_out, code_out, output_out = session.run([x, code, outputs], {x: np.expand_dims(train_images[i], axis=0)})
                 psnr_list.append(util.psnr(train_images[i],output_out))
             avg_psnr = np.sum(psnr_list) / len(psnr_list)
-        
+
+            if not validate:
+                encoder_saver.save(session, os.path.join(save_dir + "models/", "maxquality_encoder_homework_3-0"))
+                decoder_saver.save(session, os.path.join(save_dir + "models/", "maxquality_decoder_homework_3-0"))
+                encoder_saver.save(session, os.path.join(save_dir + "models/", "maxcompression_encoder_homework_3-0"))
+                decoder_saver.save(session, os.path.join(save_dir + "models/", "maxcompression_decoder_homework_3-0"))
+
         allfile = open('output/all_models_out.csv', 'a+')
         allfile.write(ae_name + ',' +str(code_size) + ',' + str(sparsity_weight) + ',' + str(batch_size) + ',' + str(epoch) + ',' + str(avg_psnr) +"\n")
         allfile.close()
