7e78443459d70b20db26354c030eb0cf797a45eb
charmse <caleharms@protonmail.ch>
2018-03-13 15:40:12 -0500

    Update .py files

--- homework/03/model.py
+++ homework/03/model.py
@@ -104,6 +104,7 @@ def transfer(model_name):
     return x, output, arch
 
 def autoencoder_network(x, code_size, model):
+    
     if(model == 'default'):
         #x = tf.placeholder(tf.float32, [None, 32, 32, 3], name='encoder_input')
         encoder_16 = downscale_block(x, filter=np.floor(x.get_shape().as_list()[3] * 1.25))
@@ -119,6 +120,20 @@ def autoencoder_network(x, code_size, model):
         decoder_8 = tf.reshape(hidden_decoder, [-1, 8, 8, 3])
         decoder_8 = upscale_block(decoder_8)
         outputs = upscale_block(decoder_8)
+        tf.identity(outputs, name='decoder_output')
+    elif(model == 'maxcompression'):
+        en_conv_64 = tf.layers.conv2d(x, 64, 3, strides=2, padding='same',activation = tf.nn.elu)
+        en_conv_128 = tf.layers.conv2d(en_conv_64, 128, 3, strides= 2, padding='same', activation=tf.nn.elu)
+        flatten_dim = np.prod(en_conv_128.get_shape().as_list()[1:])
+        flat = tf.reshape(en_conv_128, [-1, flatten_dim])
+        en_dense_1 = tf.layers.dense(flat, 100000, activation=tf.nn.elu)
+        en_dense_2 = tf.layers.dense(en_dense_1, 10000, activation=tf.nn.elu)
+        code = tf.layers.dense(en_dense_2, 1000, activation=tf.nn.elu, name='encoder_output')
+        de_dense_1 = tf.layers.dense(flat, 10000, activation=tf.nn.elu, name='decoder_input')
+        de_dense_2 = tf.layers.dense(en_dense_2, 100000, activation=tf.nn.elu)
+        de_trans_128 = tf.layers.conv2d_transpose(tf.reshape(de_dense_2, [-1, 4, 4, 3]), 128, 3, strides=(2, 2), padding='same', activation=tf.nn.elu)
+        de_trans_64 = tf.layers.conv2d_transpose(de_trans_128, 64, 3, strides=(2, 2), padding='same', activation=tf.nn.elu)
+        outputs = tf.layers.conv2d_transpose(de_trans_64, 3, 3, strides=(2,2), padding='same', activation=tf.nn.elu)
         tf.identity(outputs, name='decoder_output')
     else:
         print("Error: Auto Encoder Model Not Found!")
