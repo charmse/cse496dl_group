a70102a220066eed6dc6fd70ae1ec4bf74e271d6
charmse <caleharms@protonmail.ch>
2018-03-13 19:01:12 -0500

    Update main.py

--- homework/03/main.py
+++ homework/03/main.py
@@ -124,8 +124,8 @@ def main(argv):
         #Run a test
         psnr_list = []
         for i in range(test_num_examples): 
-            x_out, code_out, output_out = session.run([x, code, outputs], {x: np.expand_dims(train_images[i], axis=0)})
-            psnr_list.append(util.psnr(train_images[i],output_out))
+            x_out, code_out, output_out = session.run([x, code, outputs], {x: np.expand_dims(test_images[i], axis=0)})
+            psnr_list.append(util.psnr(test_images[i],output_out))
         avg_psnr = np.sum(psnr_list) / len(psnr_list)
 
         if not validate:
