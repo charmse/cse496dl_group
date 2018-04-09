922fe135aff59c15e8e13159b71b9d95610ffb75
charmse <caleharms@protonmail.ch>
2018-04-08 23:17:04 -0500

    Update main

--- homework/04/main.py
+++ homework/04/main.py
@@ -63,20 +63,18 @@ def main(argv):
     # Update the cost
     cost = tf.reduce_sum(loss)
 
-        # get the prediction accuracy
+    # get the prediction accuracy
     softmax_out = tf.nn.softmax(tf.reshape(logits, [-1, VOCAB_SIZE]))
     predict = tf.cast(tf.argmax(softmax_out, axis=1), tf.int32)
     correct_prediction = tf.equal(predict, tf.reshape(training_input.targets, [-1]))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-
-    #self.learning_rate = tf.Variable(0.0, trainable=False)
     optimizer = tf.train.RMSPropOptimizer(LEARNING_RATE)
     train_op = optimizer.minimize(loss)
 
     print_iter = 50
     init_op = tf.global_variables_initializer()
-    #orig_decay = lr_decay
+    
     with tf.Session() as sess:
         # start threads
         sess.run([init_op])
@@ -88,13 +86,11 @@ def main(argv):
             current_state = np.zeros((LSTM_LAYERS, 2, BATCH_SIZE, LSTM_SIZE))
 
             for step in range(training_input.epoch_size):
-                # cost, _ = sess.run([m.cost, m.optimizer])
+                
                 if step % print_iter != 0:
                     cost1, _, current_state = sess.run([cost, train_op, state],
                                                     feed_dict={init_state: current_state})
                 else:
-                    #seconds = (float((dt.datetime.now() - curr_time).seconds) / print_iter)
-                    #curr_time = dt.datetime.now()
                     cost1, _, current_state, acc = sess.run([cost, train_op, state, accuracy],
                                                         feed_dict={init_state: current_state})
                     print("Epoch {}, Step {}, cost: {:.3f}, accuracy: {:.3f}".format(epoch,
@@ -144,7 +140,7 @@ def main(argv):
             acc_check_thresh = 5
             accuracy_final = 0
             for batch in range(test_input.epoch_size):
-                    current_state = np.zeros((1, 2, m_test.batch_size, m_test.hidden_size))
+                    current_state = np.zeros((LSTM_LAYERS, 2, m_test.batch_size, m_test.hidden_size))
                     data_input,true_vals, pred, current_state, acc = sess.run([m_test.input_obj.input_data,m_test.input_obj.targets, predict, m_test.state, accuracy],
                                                                 feed_dict={m_test.init_state: current_state})
                     pred_string = [reversed_dictionary[x] for x in pred[:m_test.num_steps]]
@@ -161,69 +157,6 @@ def main(argv):
             # close threads
             coord.request_stop()
             coord.join(threads)
-
-    # raw_data = ptb_reader.ptb_raw_data(DATA_DIR)
-    # train_data, valid_data, test_data, _ = raw_data
-    # train_input = PTBInput(train_data, BATCH_SIZE, TIME_STEPS, name="TrainInput")
-    # test_input = PTBInput(test_data,BATCH_SIZE,TIME_STEPS,name="TestInput")
-
-    # # setup input and embedding
-    # embedding_matrix = tf.get_variable('embedding_matrix', dtype=tf.float32, shape=[VOCAB_SIZE, EMBEDDING_SIZE], trainable=True)
-    # word_embeddings = tf.nn.embedding_lookup(embedding_matrix, train_input.input_data)
-
-    # lstm_cell = tf.contrib.rnn.BasicLSTMCell(LSTM_SIZE)
-
-    # # Initial state of the LSTM memory.
-    # initial_state = lstm_cell.zero_state(BATCH_SIZE, tf.float32)
-    
-    # outputs, state = tf.nn.dynamic_rnn(lstm_cell, word_embeddings,
-    #                                initial_state=initial_state,
-    #                                dtype=tf.float32)
-
-    # logits = tf.layers.dense(outputs, VOCAB_SIZE)
-
-    # loss = tf.contrib.seq2seq.sequence_loss(
-    #     logits,
-    #     train_input.targets,
-    #     tf.ones([BATCH_SIZE, TIME_STEPS], dtype=tf.float32),
-    #     average_across_timesteps=True,
-    #     average_across_batch=True)
-
-    # optimizer = tf.train.RMSPropOptimizer(LEARNING_RATE)
-    # train_op = optimizer.minimize(loss)
-    # softmax_out = tf.nn.softmax(tf.reshape(logits, [-1, VOCAB_SIZE]))
-    # predict = tf.cast(tf.argmax(softmax_out, axis=1), tf.int32)
-
-    # with tf.Session() as session:
-    #     session.run(tf.global_variables_initializer())
-
-    #     # start queue runners
-    #     coord = tf.train.Coordinator()
-    #     threads = tf.train.start_queue_runners(sess=session, coord=coord)
-
-    #     # retrieve some data to look at
-    #     examples = session.run([train_input.input_data, train_input.targets])
-
-    #     #train
-    #     for epoch in range(EPOCHS):
-    #         for i in range(train_input.input_data.shape[0] // BATCH_SIZE):
-    #             _ = session.run(train_op)
-
-    #     a = session.run([test_input.input_data[0]])
-    #     b = session.run([predict])
-
-    #     correct_prediction = tf.equal(predict, tf.reshape(test_input.targets, [-1]))
-    #     session.run(correct_prediction[0])
-    #     session.run(correct_prediction)
-
-    #     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    #     accuracy_val = session.run(accuracy)
-
-    # allfile = open('output/all_models_out.csv', 'a+')
-    # allfile.write(str(LSTM_SIZE) + ',' + str(k) + ',' + str(VOCAB_SIZE) + ',' + str(EMBEDDING_SIZE) + ',' + str(EPOCHS) + ',' + str(BATCH_SIZE) + ',' + str(TIME_STEPS) + ',' + str(LEARNING_RATE) + ',' + str(accuracy_val) +"\n")
-    # allfile.close()
-
-
 
 class PTBInput(object):
         """The input data.
